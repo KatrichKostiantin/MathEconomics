25b4af8a12d2e99926070edf6cf71fbef73279b6
KastorRico <sandra.pylypiva@gmail.com>
2022-02-13 21:31:59 +0200

    Some changes in lab4

--- Lab4.R
+++ Lab4.R
@@ -5,29 +5,26 @@ Y <- data$rating
 
 x1 <- data$calories
 x2 <- data$protein
-x3 <- data$sodium
-x4 <- data$cups
+x3 <- data$cups
 
 #### (A) ####
 
-mod <- lm(Y ~ x1+x2+x3+x4)
+mod <- lm(Y ~ x1+x2+x3)
+sumMod <- summary(mod)
 
 #### (B) ####
-
-summary(mod)
-
-#### (C) ####
 
 # Знаходимо t_k для моделі
 alpha <- 0.05
 n <- length(data$calories)
-p <- 4
+p <- 3
 t_k <- qt(p = 1 - alpha / 2, df = n - p - 1)
 t_k
+# [1] 1.992997
 
-# [1] 1.993464
+sumMod$coefficients
 
-#### (D) ####
+#### (С) ####
 
 confint(mod, level = 0.90)
 
@@ -44,18 +41,29 @@ predict(mod, interval = "confidence", level = 0.95)
 
 predict(mod, interval = "confidence", level = 0.99)
 
-#### (F) ####
+#### (E) ####
 
+# прогноз для середнього
 mean(x1)
 mean(x2)
 mean(x3)
-mean(x4)
-dataNewMean <- data.frame(x1 = 107,x2=2.5 ,x3=160 ,x4=0.8 )
+dataNewMean <- data.frame(x1 = 107,x2=2.5 ,x3=0.821)
 predict(mod, newdata = dataNewMean, interval = "prediction")
+
+# прогноз на наступний період
 max(x1)*1.1
 max(x2)*1.1
 max(x3)*1.1
-max(x4)*1.1
-dataNewMax <- data.frame(x1 = 176,x2=6.6 ,x3=352 ,x4=1.65 )
+dataNewMax <- data.frame(x1 = 176,x2=6.6 ,x3=1.65)
 predict(mod, newdata = dataNewMax, interval = "prediction")
 
+#### (F) ####
+
+# Перевіряємо F-statistic по summary
+sumMod
+
+#### (G) ####
+
+# Видаляємо неважливий фактор(x3), він є неважливим, бо його можна занулити
+
+mod2 <- lm(Y ~ x1+x2)
