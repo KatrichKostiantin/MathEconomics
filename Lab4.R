a9a412818ac5d003e7077fb0939b8effb34917b1
KastorRico <sandra.pylypiva@gmail.com>
2022-02-15 17:10:30 +0200

    Finished Lab4

--- Lab4.R
+++ Lab4.R
@@ -4,27 +4,34 @@ data <- read.csv("cereal.csv")
 Y <- data$rating
 
 x1 <- data$calories
-x2 <- data$protein
-x3 <- data$cups
+x2 <- data$sodium
+x3 <- data$protein
+x4 <- data$cups
 
 #### (A) ####
 
-mod <- lm(Y ~ x1+x2+x3)
+mod <- lm(Y ~ x1+x2+x3+x4)
 sumMod <- summary(mod)
+sumMod
 
 #### (B) ####
+
+# Перевіряємо F-statistic по summary
+sumMod
+
+#### (С) ####
 
 # Знаходимо t_k для моделі
 alpha <- 0.05
 n <- length(data$calories)
-p <- 3
+p <- 4
 t_k <- qt(p = 1 - alpha / 2, df = n - p - 1)
 t_k
-# [1] 1.992997
+# [1] 1.993464
 
 sumMod$coefficients
 
-#### (С) ####
+#### (D) ####
 
 confint(mod, level = 0.90)
 
@@ -41,57 +48,20 @@ predict(mod, interval = "confidence", level = 0.95)
 
 predict(mod, interval = "confidence", level = 0.99)
 
-#### (E) ####
+#### (F) ####
 
 # прогноз для середнього
 mean(x1)
 mean(x2)
 mean(x3)
-dataNewMean <- data.frame(x1 = 107,x2=2.5 ,x3=0.821)
-predict(mod, newdata = dataNewMean, interval = "prediction")
+mean(x4)
+dataNewMean <- data.frame(x1 = 106.8831,x2=159.6753 ,x3=2.545455, x4 = 0.821039)
+predict(mod, newdata = dataNewMean, interval = "confidence")
 
 # прогноз на наступний період
 max(x1)*1.1
 max(x2)*1.1
 max(x3)*1.1
-dataNewMax <- data.frame(x1 = 176,x2=6.6 ,x3=1.65)
+max(x4)*1.1
+dataNewMax <- data.frame(x1 = 176,x2=352 ,x3=6.6, x4= 1.65)
 predict(mod, newdata = dataNewMax, interval = "prediction")
-
-#### (F) ####
-
-# Перевіряємо F-statistic по summary
-sumMod
-
-#### (G) ####
-
-# Видаляємо неважливий фактор(x3), він є неважливим, бо його можна занулити
-mod2 <- lm(Y ~ x1 + x2)
-summary(mod2)
-
-#### (H) ####
-
-mod2 <- lm(Y ~ x1 + x2)
-summ2 <- summary(mod2)
-summ2$r.squared
-summ2$adj.r.squared
-
-mod3 <- lm(Y ~ x1 + x3)
-summ3 <- summary(mod3)
-summ3$r.squared
-summ3$adj.r.squared
-
-mod4 <- lm(Y ~ x2 + x3)
-summ4 <- summary(mod4)
-summ4$r.squared
-summ4$adj.r.squared
-
-
-#### (I) ####
-mod5 <- lm(Y ~ x1)
-summ5 <- summary(mod5)
-summ5$r.squared
-
-mod6 <- lm(Y ~ x2)
-summ6 <- summary(mod6)
-summ6$r.squared
-
